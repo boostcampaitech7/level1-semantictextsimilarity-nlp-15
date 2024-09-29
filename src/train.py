f38fc5e29dd006a7f4da8087c48bd7b83e1b8290
simigami <46891822+simigami@users.noreply.github.com>
2024-09-29 14:40:24 +0900

    update: Merge-fix between enhance_2 <-> gyutae

--- src/train.py
+++ src/train.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-import argparse
-import os
->>>>>>> 1b57967bc99b9920498a9b37430d3281725fbe8c
 import os
 import argparse
 import random
--- src/util/util.py
+++ src/util/util.py
@@ -21,13 +21,7 @@ class Tokenizer(Dataset):
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, tokenizer, aug_list, path, train=False, predict=False):
-<<<<<<< HEAD
         self.data = pd.read_csv(path) if not isinstance(path, pd.DataFrame) else path
-=======
-        self.data = None
-        
-        self.data = pd.read_csv(path)
->>>>>>> 1b57967bc99b9920498a9b37430d3281725fbe8c
         self.aug_list = aug_list
         self.tokenizer = tokenizer
         self.predict = predict
@@ -43,10 +37,6 @@ class Dataset(torch.utils.data.Dataset):
         s2 = self.data.iloc[idx]['sentence_2']
 
         enc = self.tokenizer(s1, s2, return_tensors='pt', truncation=True)
-<<<<<<< HEAD
-
-=======
->>>>>>> 1b57967bc99b9920498a9b37430d3281725fbe8c
         mapper = {key : value.squeeze(0) for key, value in enc.items()} # value Vector (1, token_size) -> (token_size)
 
         if not self.predict:
@@ -63,47 +53,13 @@ class Dataset(torch.utils.data.Dataset):
 
         if 'remove_special' in self.aug_list:
             self.data = data_augmentation.remove_special_characters(self.data)
-
-<<<<<<< HEAD
+        
         if 'copy_sentence' in self.aug_list:
             self.data = pd.concat([self.data, data_augmentation.copy_sentence(self.data)], ignore_index=True)
 
         if 'under_sampling' in self.aug_list:
             self.data = data_augmentation.under_sampling(self.data)
-# class k_fold_dataset(torch.utils.data.Dataset):
-#     def __init__(self, tokenizer, s1, s2, labels, aug_list, max_length=128):
-#         self.s1 = s1.tolist() if isinstance(s1, pd.Series) else s1
-#         self.s2 = s2.tolist() if isinstance(s2, pd.Series) else s2
-#         self.labels = labels.tolist() if isinstance(labels, pd.Series) else labels
-#
-#         self.tokenizer = tokenizer
-#         self.max_length = max_length
-#         self.aug_list = aug_list
-#
-#         self.aug_for_dataset_class()
-#
-#     def __len__(self):
-#         return len(self.s1)
-#
-#     def __getitem__(self, idx):
-#         s1 = self.data.iloc[idx]
-#         s2 = self.data.iloc[idx]
-#
-#         enc = self.tokenizer(s1, s2, return_tensors='pt', truncation=True)
-#
-#         mapper = {key: value.squeeze(0) for key, value in enc.items()}  # value Vector (1, token_size) -> (token_size)
-#         return mapper
-#
-#     def aug_for_dataset_class(self):
-#         if 'swap' in self.aug_list:
-#             self.data = data_augmentation.swap_sentences(self.data)
-#
-#         if 'koeda' in self.aug_list:
-#             self.data = data_augmentation.koeda(self.data)
-#
-#         if 'remove_special' in self.aug_list:
-#             self.data = data_augmentation.remove_special_characters(self.data)
-=======
+
 class k_fold_dataset(torch.utils.data.Dataset):
     def __init__(self, tokenizer, s1, s2, labels, aug_list, max_length=128):
         self.s1 = s1.tolist() if isinstance(s1, pd.Series) else s1
@@ -137,7 +93,12 @@ class k_fold_dataset(torch.utils.data.Dataset):
 
         if 'remove_special' in self.aug_list:
             self.data = data_augmentation.remove_special_characters(self.data)
->>>>>>> 1b57967bc99b9920498a9b37430d3281725fbe8c
+        
+        if 'copy_sentence' in self.aug_list:
+            self.data = pd.concat([self.data, data_augmentation.copy_sentence(self.data)], ignore_index=True)
+
+        if 'under_sampling' in self.aug_list:
+            self.data = data_augmentation.under_sampling(self.data)
 
 class Dataloader(pl.LightningDataModule):
     def __init__(self, model_name, aug_list, batch_size, max_length, num_worker, train_path, val_path, dev_path, predict_path):
@@ -160,29 +121,12 @@ class Dataloader(pl.LightningDataModule):
             self.train_dataset, self.val_dataset = data_augmentation.k_fold_split(train_val_concat, kf, self.tokenizer.tokenizer)
             self.dev_dataset = Dataset(self.tokenizer.tokenizer, aug_list, self.dev_path)
             self.predict_dataset = Dataset(self.tokenizer.tokenizer, aug_list, self.predict_path, predict=True)
-<<<<<<< HEAD
-            return
-
-        elif 'train_val_split' in aug_list:
-            train_dataset, val_dataset = data_augmentation.train_val_split(self.train_path, self.val_path)
-
-            self.train_dataset = Dataset(self.tokenizer.tokenizer, aug_list, train_dataset, train=True)
-            self.val_dataset = Dataset(self.tokenizer.tokenizer, aug_list, val_dataset)
-            self.dev_dataset = Dataset(self.tokenizer.tokenizer, aug_list, self.dev_path)
-            self.predict_dataset = Dataset(self.tokenizer.tokenizer, aug_list, self.predict_path, predict=True)
-            return
-=======
->>>>>>> 1b57967bc99b9920498a9b37430d3281725fbe8c
 
         else:
             self.train_dataset = Dataset(self.tokenizer.tokenizer, aug_list, self.train_path, train=True)
             self.val_dataset = Dataset(self.tokenizer.tokenizer, aug_list, self.val_path)
             self.dev_dataset = Dataset(self.tokenizer.tokenizer, aug_list, self.dev_path)
             self.predict_dataset = Dataset(self.tokenizer.tokenizer, aug_list, self.predict_path, predict=True)
-<<<<<<< HEAD
-            return
-=======
->>>>>>> 1b57967bc99b9920498a9b37430d3281725fbe8c
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(self.train_dataset, persistent_workers=True, batch_size=self.batch_size, shuffle=True, num_workers=self.num_worker, collate_fn=self.collate)
